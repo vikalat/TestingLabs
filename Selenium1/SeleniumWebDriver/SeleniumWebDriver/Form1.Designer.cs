a942e4d847c7100636f9bded46693e529895bcd3
vikalat <49753228+vikalat@users.noreply.github.com>
2019-11-29 21:01:38 +0300

    Selenium

---
+++ Selenium1/SeleniumWebDriver/SeleniumWebDriver/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace SeleniumWebDriver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartFirstTestButtonClick = new System.Windows.Forms.Button();
+            this.StopFirstTestButton = new System.Windows.Forms.Button();
+            this.StartSecondTestButton = new System.Windows.Forms.Button();
+            this.StopSecondTestButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // StartFirstTestButtonClick
+            // 
+            this.StartFirstTestButtonClick.Location = new System.Drawing.Point(104, 122);
+            this.StartFirstTestButtonClick.Name = "StartFirstTestButtonClick";
+            this.StartFirstTestButtonClick.Size = new System.Drawing.Size(160, 88);
+            this.StartFirstTestButtonClick.TabIndex = 0;
+            this.StartFirstTestButtonClick.Text = "Test1";
+            this.StartFirstTestButtonClick.UseVisualStyleBackColor = true;
+            this.StartFirstTestButtonClick.Click += new System.EventHandler(this.StartFirstTestButtonClick_Click);
+            // 
+            // StopFirstTestButton
+            // 
+            this.StopFirstTestButton.Location = new System.Drawing.Point(104, 247);
+            this.StopFirstTestButton.Name = "StopFirstTestButton";
+            this.StopFirstTestButton.Size = new System.Drawing.Size(160, 94);
+            this.StopFirstTestButton.TabIndex = 1;
+            this.StopFirstTestButton.Text = "StopTest1";
+            this.StopFirstTestButton.UseVisualStyleBackColor = true;
+            this.StopFirstTestButton.Click += new System.EventHandler(this.StopFirstTestButton_Click);
+            // 
+            // StartSecondTestButton
+            // 
+            this.StartSecondTestButton.Location = new System.Drawing.Point(307, 122);
+            this.StartSecondTestButton.Name = "StartSecondTestButton";
+            this.StartSecondTestButton.Size = new System.Drawing.Size(165, 88);
+            this.StartSecondTestButton.TabIndex = 2;
+            this.StartSecondTestButton.Text = "Test2";
+            this.StartSecondTestButton.UseVisualStyleBackColor = true;
+            this.StartSecondTestButton.Click += new System.EventHandler(this.StartSecondTestButton_Click);
+            // 
+            // StopSecondTestButton
+            // 
+            this.StopSecondTestButton.Location = new System.Drawing.Point(307, 247);
+            this.StopSecondTestButton.Name = "StopSecondTestButton";
+            this.StopSecondTestButton.Size = new System.Drawing.Size(160, 94);
+            this.StopSecondTestButton.TabIndex = 3;
+            this.StopSecondTestButton.Text = "StopTest2";
+            this.StopSecondTestButton.UseVisualStyleBackColor = true;
+            this.StopSecondTestButton.Click += new System.EventHandler(this.StopSecondTestButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.StopSecondTestButton);
+            this.Controls.Add(this.StartSecondTestButton);
+            this.Controls.Add(this.StopFirstTestButton);
+            this.Controls.Add(this.StartFirstTestButtonClick);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartFirstTestButtonClick;
+        private System.Windows.Forms.Button StopFirstTestButton;
+        private System.Windows.Forms.Button StartSecondTestButton;
+        private System.Windows.Forms.Button StopSecondTestButton;
+    }
+}
+
---
+++ Selenium1/SeleniumWebDriver/SeleniumWebDriver/Form1.cs
@@ -0,0 +1,65 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeleniumWebDriver
+{
+    public partial class Form1 : Form
+    {
+        IWebDriver BrouserGoogleChrome;
+        IWebDriver BrouserGoogleChromeSecondTest;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartFirstTestButtonClick_Click(object sender, EventArgs e)
+        {
+            //№2  Тестирование строки поиска товара
+            BrouserGoogleChrome = new OpenQA.Selenium.Chrome.ChromeDriver();
+            BrouserGoogleChrome.Manage().Window.Maximize();
+            BrouserGoogleChrome.Navigate().GoToUrl("https://deal.by/");
+
+            IWebElement SearhInputLine = BrouserGoogleChrome.FindElement(By.Name("search_term"));
+
+            SearhInputLine.SendKeys("еда" + OpenQA.Selenium.Keys.Enter);
+            //Ожидаемый результат: произойдет перенаправление на список ссылок относящийся к слову еда
+        }
+
+        private void StopFirstTestButton_Click(object sender, EventArgs e)
+        {
+            BrouserGoogleChrome.Quit();
+        }
+
+        private void StartSecondTestButton_Click(object sender, EventArgs e)
+        {
+            //№3  Тестирование фильтрации услуг
+            BrouserGoogleChromeSecondTest = new OpenQA.Selenium.Chrome.ChromeDriver();
+            BrouserGoogleChromeSecondTest.Manage().Window.Maximize();
+            BrouserGoogleChromeSecondTest.Navigate().GoToUrl("https://deal.by/");
+
+            IWebElement SearhInputLine = BrouserGoogleChromeSecondTest.FindElement(By.Name("search_term"));
+
+            SearhInputLine.SendKeys("еда" + OpenQA.Selenium.Keys.Enter);
+            Thread.Sleep(6000);
+            IWebElement SearhInputLine2 = BrouserGoogleChromeSecondTest.FindElement(By.XPath("//span[text()='Ланч боксы']"));
+
+            SearhInputLine2.Click();
+            //Ожидаемый результат: на странице будут присутствовать только ланч боксы
+        }
+
+        private void StopSecondTestButton_Click(object sender, EventArgs e)
+        {
+            BrouserGoogleChromeSecondTest.Quit();
+        }
+    }
+}
