e85f88deaadc56f491596b8719cc5b3c340cef98
vikalat <49753228+vikalat@users.noreply.github.com>
2019-11-30 08:23:52 +0300

    PageObject

---
+++ lab6/PageObject/Page/MainPage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace PageObject.Page
+{
+    class MainPage
+    {
+       
+
+        [FindsBy(How = How.Name, Using = "search_term")]
+        private IWebElement Poisk;
+
+        public MainPage(IWebDriver browser)
+        {
+            PageFactory.InitElements(browser, this);
+        }
+
+        public MainPage InputPoisk(string q)
+        {
+            Poisk.SendKeys(q);
+            Poisk.SendKeys(Keys.Enter);          
+            return this;
+        }
+
+        
+
+        [FindsBy(How = How.XPath, Using = @"//span[text()='Ланч боксы']")]
+        private IWebElement inputFilter;
+
+        public MainPage InputFilter(string filt)
+        {
+            inputFilter.FindElement(By.XPath("//span[text()='" + filt + "']"));
+            inputFilter.Click();            
+            return this;
+        }
+
+
+
+    }
+}
---
+++ lab6/PageObject/Test.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PageObject.Page;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace PageObject
+{
+    [TestClass]
+    public class Test
+    {
+        IWebDriver Browser;
+        private static string HomePage = "https://deal.by/";
+
+        [TestMethod]
+        public void TestSearch()
+        {
+            Browser = new ChromeDriver();
+            Browser.Navigate().GoToUrl(HomePage);
+
+            
+
+            MainPage orderCarPage = new MainPage(Browser)
+                .InputPoisk("еда");
+           
+           
+            Browser.Quit();
+        }
+
+        [TestMethod]
+        public void TestFilter()
+        {
+            Browser = new ChromeDriver();
+            Browser.Navigate().GoToUrl(HomePage);
+
+            MainPage orderCarPage = new MainPage(Browser)
+                .InputPoisk("еда")
+                .InputFilter("Ланч боксы");
+
+            Browser.Quit();
+        }
+    }
+}
